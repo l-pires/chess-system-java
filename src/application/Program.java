f1e23518aaf7817be98e69974f8211579956723d
Lucas Pereira <pereirapas123@gmail.com>
2020-04-02 14:44:59 -0300

    Check logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,10 @@
 package application;
 
-import java.util.ArrayList;
 import java.util.InputMismatchException;
-import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
 import chess.ChessMatch;
-import chess.ChessPiece;
 import chess.ChessPosition;
 
 public class Program {
